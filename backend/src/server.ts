339710d857c05c85fe886fa319d23cb93ae1ed01
Jeanball <jeanball44@hotmail.com>
2025-06-21 23:56:04 -0400

    Initial setup of Frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,24 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
 import usersRoute from "./routes/usersRoute"
 import { connectDB } from "./config/db"
-import dotenv from "dotenv"
+
+
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
 
+app.use(cors({
+    origin:"http://localhost:5173",
+}))
 app.use(express.json());
 app.use("/api/users", usersRoute)
+
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
