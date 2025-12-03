3b42d156fcf2ea1c3d5285ce701129b24f81d433
Jeanball <jeanball44@hotmail.com>
2025-12-02 21:21:37 -0500

    Add logout button

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -60,6 +60,12 @@ const Navbar: React.FC<NavbarProps> = ({ username, onLogout }) => {
           </svg>
           <span className="btm-nav-label">Settings</span>
         </Link>
+        <button onClick={onLogout} className="flex flex-col items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+          </svg>
+          <span className="btm-nav-label">Logout</span>
+        </button>
       </div>
     </>
   );
